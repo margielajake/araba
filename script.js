ac6c29aabfe4b1e5425e902c1991aa75a4c9b0d9
margielajake <campbell.jake14@gmail.com>
2023-10-31 14:54:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let countDownDate = new Date("Jan 01, 2024 00:00:00").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let timeLeft = countDownDate - now;
+
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("days").innerHTML = days;
+}, 1000);
